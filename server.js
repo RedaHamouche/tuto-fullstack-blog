f800a030d1941c6b9cac2ea1386364466f758e9e
RedaHamouche <reda.hamouche@outlook.com>
2020-08-07 19:13:08 +0200

    put done 📝

--- server.js
+++ server.js
@@ -1,22 +1,36 @@
 const express = require("express");
 const app = express();
 const articlesrouter = require("./routes/articles");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv/config");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.json());
 
-app.use("/articles", articlesrouter);
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   const articles = [
     {
       title: "Test Article",
-      createdAt: Date.now(),
+      createdAt: new Date(),
+      description: "Test description"
+    },
+    {
+      title: "Test Article",
+      createdAt: new Date(),
       description: "Test description"
     }
   ];
-  res.render("index", {
+  res.render("articles/index", {
     articles: articles
   });
 });
 
+app.use("/articles", articlesrouter);
 app.listen(3000);
+mongoose.connect(process.env.DB_CONNECTION, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
