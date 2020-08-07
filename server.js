038bb6815be747e967022570266e734377457cef
RedaHamouche <reda.hamouche@outlook.com>
2020-08-07 20:53:56 +0200

    crud done but edit bug

--- server.js
+++ server.js
@@ -4,15 +4,19 @@ const articlesrouter = require("./routes/articles");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const Article = require("./models/article");
+const methodOveeride = require("method-override");
 require("dotenv/config");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
+app.use(methodOveeride("_method"));
 
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", async (req, res) => {
-  const articles = await Article.find();
+  const articles = await Article.find().sort({
+    createdAt: "desc"
+  });
 
   res.render("articles/index", {
     articles: articles
@@ -23,5 +27,6 @@ app.use("/articles", articlesrouter);
 app.listen(3000);
 mongoose.connect(process.env.DB_CONNECTION, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true
 });
